c6a9371926e4b9cb622fdca5d814f40c174aa969
huntikins <hunterttrammell@gmail.com>
2024-07-29 14:59:10 -0500

    [chore] more styles

--- src/components/navbar/partials/menu.php
+++ src/components/navbar/partials/menu.php
@@ -1,8 +1,7 @@
-<nav class="absolute top-0 left-0 w-screen h-screen p-8 sm:mx-auto text-light-primary sm:max-w-screen-2xl sm:p-12 bg-dark-primary/40 backdrop-blur-md"
-    id="primary-menu" :class="!isOpen ? 'hidden' : false" x-trap="isOpen">
-    <div class="absolute top-0 right-0 flex items-center justify-end h-24 px-12 py-2">
-        <button class="w-10 h-10 text-brand-light stroke-brand-light" aria-controls="primary-menu"
-            :aria-expanded="isOpen" aria-label="Open menu" @click="isOpen = !isOpen" type="button">
+<nav class="bm:nav__menu" id="primary-menu" :hidden="!isOpen" x-trap="isOpen">
+    <div class="bm:nav__menu-button">
+        <button class="bm:btn bm:btn-menu" aria-controls="primary-menu" :aria-expanded="isOpen" aria-label="Open menu"
+            @click="isOpen = !isOpen" type="button">
             <?php get_template_part('assets/icons/menu'); ?>
         </button>
     </div>
