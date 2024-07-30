7832357d7c62066a1d792d8828faac75bb7232b5
huntikins <hunterttrammell@gmail.com>
2024-07-29 20:41:08 -0500

    [chore] added card + partial hero

--- src/components/card/card.php
+++ src/components/card/card.php
@@ -1,50 +1,48 @@
-<article class="relative w-full h-full shadow-2xl text-light-primary">
-    <div class="absolute top-0 left-0 w-full h-full">
+<article class="bm:card">
+    <div class="bm:card__bg">
         <!-- The post image -->
-        <?php the_post_thumbnail('post-thumbnail', ['class' => 'block object-cover w-full h-full sm:object-left brightness-50 saturate-75 contrast-75']); ?>
+        <?php the_post_thumbnail('post-thumbnail', ['class' => 'bm:card__bg--image']); ?>
     </div>
-    <div class="relative z-10 flex flex-col h-full p-6 sm:p-12 bg-dark-primary/60">
-        <div class="flex items-center justify-start pb-6">
+    <div class="bm:card__content">
+        <div class="bm:card__content--heading">
             <!--Author-->
-            <span class="inline-block w-10 h-10 mr-3">
+            <span class="bm:card__avatar">
                 <?php echo get_avatar(get_the_author_meta('ID'), $size = '60', $default = 'mystery', $alt = '', $args = array('class' => 'rounded-full')); ?>
             </span>
-            <span class="font-mono text-base">
-                <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>"
-                    title="<?php echo esc_attr(get_the_author()); ?>">
-                    @<?php the_author(); ?>
-                </a>
-            </span>
+            <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>"
+                title="<?php echo esc_attr(get_the_author()); ?>">
+                @<?php the_author(); ?>
+            </a>
         </div>
-        <div class="py-12">
-            <strong class="pb-2 font-mono text-sm italic sm:text-xs text-light-primary">
-                <span class="text-brand-dark" aria-hidden="true">//</span>
+        <div class="bm:card__content--description">
+            <strong class="bm:card__date">
+                <span class="bm:accent" aria-hidden="true">//</span>
                 <span><?php echo get_the_date('F jS, Y'); ?></span>
             </strong>
-            <h3 class="pb-2 text-2xl font-bold"><?php the_title(); ?></h3>
+            <h3 class="bm:card__title"><?php the_title(); ?></h3>
             <p>
                 <?php echo get_the_excerpt(); ?>
             </p>
         </div>
-        <div class="flex flex-col mt-auto">
-            <div class="w-full">
-                <div class="flex items-center justify-start">
+        <div class="bm:card__content--info">
+            <div class="bm:card__info">
+                <div class="bm:card__info--details">
                     <span class="sr-only">Reading time </span>
-                    <span class="inline-block w-4 h-4 mr-2"
+                    <span class="bm:icon bm:icon-small bm:mr-2"
                         aria-hidden="true"><?php get_template_part('assets/icons/read'); ?></span>
                     <span>
                         <?php echo the_reading_time(the_post()); ?>
                     </span>
                 </div>
-                <hr class="my-3 border-1 border-light-primary/70">
-                <span class="text-base">
+                <hr class="bm:divider">
+                <span>
                     <?php
                     $post_tags = get_the_tags();
 
                     if (!empty($post_tags)) {
                         foreach ($post_tags as $post_tag) {
                             ?>
-                            <span class="mr-2">
+                            <span class="bm:mr-2">
                                 <a
                                     href="<?php echo get_term_link($post_tag, $post_tag->name); ?>">#<?php echo $post_tag->name; ?></a>
                             </span>
@@ -53,7 +51,7 @@
                     } ?>
                 </span>
             </div>
-            <div class="pt-6">
+            <div class="bm:pt-4">
                 <a href="<?php the_permalink(); ?>" aria-label="Read <?php the_title(); ?>"
                     class="bm:btn bm:btn-primary">
                     <span>Read now</span>
--- src/theme/templates/partials/home/hero.php
+++ src/theme/templates/partials/home/hero.php
@@ -9,66 +9,66 @@ $featured = new WP_Query($args);
 if ($featured->have_posts()):
     while ($featured->have_posts()):
         $featured->the_post(); ?>
-        <section class="relative h-screen">
-            <div class="absolute top-0 left-0 w-screen h-screen max-w-screen-2xl" aria-hidden="true">
-                <?php the_post_thumbnail('post-thumbnail', ['class' => 'block object-cover w-full h-full sm:object-left brightness-50 saturate-75 contrast-75']); ?>
+<section class="bm:hero bm:hero-full">
+    <div class="bm:hero__bg" aria-hidden="true">
+        <?php the_post_thumbnail('post-thumbnail', ['class' => 'bm:hero__bg--image']); ?>
+    </div>
+    <div
+        class="absolute top-0 left-0 w-screen h-screen p-8 pt-24 bg-dark-primary/80 sm:p-12 sm:pt-20 text-light-primary max-w-screen-2xl">
+        <div class="flex flex-col items-center justify-center w-full h-full">
+            <div class="flex flex-col items-start justify-center w-full h-2/3">
+                <strong class="pb-2 font-mono text-xl italic text-light-primary"><span class="text-brand-dark"
+                        aria-hidden="true">//</span>
+                    Featured article</strong>
+                <h1 class="max-w-3xl text-5xl leading-tight sm:text-6xl text-brand-light"><?php the_title(); ?>
+                </h1>
             </div>
-            <div
-                class="absolute top-0 left-0 w-screen h-screen p-8 pt-24 bg-dark-primary/80 sm:p-12 sm:pt-20 text-light-primary max-w-screen-2xl">
-                <div class="flex flex-col items-center justify-center w-full h-full">
-                    <div class="flex flex-col items-start justify-center w-full h-2/3">
-                        <strong class="pb-2 font-mono text-xl italic text-light-primary"><span class="text-brand-dark"
-                                aria-hidden="true">//</span>
-                            Featured article</strong>
-                        <h1 class="max-w-3xl text-5xl leading-tight sm:text-6xl text-brand-light"><?php the_title(); ?>
-                        </h1>
-                    </div>
-                    <div class="flex items-end w-full h-1/3">
-                        <ul class="flex flex-col items-start w-full text-lg md:items-center md:flex-row">
-                            <li class="flex flex-col pb-4 pr-16 sm:pb-0">
-                                <strong class="font-mono italic text-brand-light">Author</strong>
-                                <span class="text-base">
-                                    <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>"
-                                        title="<?php echo esc_attr(get_the_author()); ?>">
-                                        @<?php the_author(); ?>
-                                    </a>
-                                </span>
-                            </li>
-                            <li class="flex flex-col pb-4 sm:pb-0 sm:pr-16">
-                                <strong class="font-mono italic text-brand-light">Published</strong>
-                                <span class="text-base"><?php echo get_the_date('F jS, Y'); ?></span>
-                            </li>
-                            <!-- <li class="flex flex-col pb-4 pr-16 sm:pb-0">
+            <div class="flex items-end w-full h-1/3">
+                <ul class="flex flex-col items-start w-full text-lg md:items-center md:flex-row">
+                    <li class="flex flex-col pb-4 pr-16 sm:pb-0">
+                        <strong class="font-mono italic text-brand-light">Author</strong>
+                        <span class="text-base">
+                            <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>"
+                                title="<?php echo esc_attr(get_the_author()); ?>">
+                                @<?php the_author(); ?>
+                            </a>
+                        </span>
+                    </li>
+                    <li class="flex flex-col pb-4 sm:pb-0 sm:pr-16">
+                        <strong class="font-mono italic text-brand-light">Published</strong>
+                        <span class="text-base"><?php echo get_the_date('F jS, Y'); ?></span>
+                    </li>
+                    <!-- <li class="flex flex-col pb-4 pr-16 sm:pb-0">
                                     <strong class="font-mono italic text-brand-light">Reading time</strong>
                                     <span class="text-base">3 minutes</span>
                                 </li> -->
-                            <li class="flex flex-col pr-8">
-                                <strong class="font-mono italic text-brand-light">Tags</strong>
-                                <span class="text-base">
-                                    <?php
+                    <li class="flex flex-col pr-8">
+                        <strong class="font-mono italic text-brand-light">Tags</strong>
+                        <span class="text-base">
+                            <?php
                                     $post_tags = get_the_tags();
 
                                     if (!empty($post_tags)) {
                                         foreach ($post_tags as $post_tag) {
                                             ?>
-                                            <span class="mr-2">
-                                                <a
-                                                    href="<?php echo get_term_link($post_tag, $post_tag->name); ?>">#<?php echo $post_tag->name; ?></a>
-                                            </span>
-                                            <?php
+                            <span class="mr-2">
+                                <a
+                                    href="<?php echo get_term_link($post_tag, $post_tag->name); ?>">#<?php echo $post_tag->name; ?></a>
+                            </span>
+                            <?php
                                         }
                                     } ?>
-                                </span>
-                            </li>
-                        </ul>
-                        <a href="<?php the_permalink(); ?>" aria-label="Read <?php the_title(); ?>" class="bm:btn bm:btn-box">
-                            <?php get_template_part('assets/icons/arrow-b-r'); ?>
-                        </a>
-                    </div>
-                </div>
+                        </span>
+                    </li>
+                </ul>
+                <a href="<?php the_permalink(); ?>" aria-label="Read <?php the_title(); ?>" class="bm:btn bm:btn-box">
+                    <?php get_template_part('assets/icons/arrow-b-r'); ?>
+                </a>
             </div>
-        </section><!-- #featured -->
-        <?php
+        </div>
+    </div>
+</section><!-- #featured -->
+<?php
     endwhile;
 else:
 endif;
